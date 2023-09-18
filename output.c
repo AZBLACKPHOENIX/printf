956c166f049accc25ec30e3b45311273e873ae53
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-09-18 12:06:56 -0700

    _printf function

--- output.c
+++ output.c
@@ -3,11 +3,13 @@
 
 int main()
 {
-char c = 'A';
-char *str = "Hello, World!";
-int percent = 42;
-_printf("%c\n", c);
-_printf("%s\n", str);
-_printf("%%\n", percent);
+int len;
+int len2;
+   _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+      _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
 return 0;
 } 
